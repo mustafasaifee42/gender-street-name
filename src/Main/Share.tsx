6b3762e6d73111c495e804ccb75f9baf0f4b35fd
mustafasaifee42 <saifee.mustafa@gmail.com>
2021-08-02 12:40:39 +0300

    fix typo

--- src/Main/Share.tsx
+++ src/Main/Share.tsx
@@ -1,8 +1,7 @@
-
-import { TwitterShareButton, TwitterIcon } from 'react-share';
-import cityData from '../data/cityData.json';
-import { CityDataType } from '../DataTypes';
-import _ from 'lodash';
+import { TwitterShareButton, TwitterIcon } from "react-share";
+import cityData from "../data/cityData.json";
+import { CityDataType } from "../DataTypes";
+import _ from "lodash";
 
 interface Props {
   darkMode: boolean;
@@ -11,38 +10,70 @@ interface Props {
 
 const ShareEl = (props: Props) => {
   const { darkMode, selectedCity } = props;
-  const cityList = ['delhi-in', 'mumbai-in', 'helsinki-fi', 'berlin-de']
+  const cityList = ["delhi-in", "mumbai-in", "helsinki-fi", "berlin-de"];
   let city = undefined;
   switch (selectedCity) {
-    case 'delhi-in':
+    case "delhi-in":
       city = "Delhi";
       break;
-    case 'mumbai-in':
+    case "mumbai-in":
       city = "Mumbai";
       break;
-    case 'helsinki-fi':
+    case "helsinki-fi":
       city = "Helsinki";
       break;
-    case 'berlin-de':
+    case "berlin-de":
       city = "Berlin";
       break;
     default:
-      city = undefined
+      city = undefined;
   }
-  const data = _.filter(cityData, (o: CityDataType) => o.city === selectedCity).length > 0 ? _.filter(cityData, (o: CityDataType) => o.city === selectedCity)[0] : null;
+  const data =
+    _.filter(cityData, (o: CityDataType) => o.city === selectedCity).length > 0
+      ? _.filter(cityData, (o: CityDataType) => o.city === selectedCity)[0]
+      : null;
 
-  const url = selectedCity ? cityList.indexOf(selectedCity) > -1 ? `https://gendered-toponyms.mustafasaifee.com/?city=${selectedCity}` : 'https://gendered-toponyms.mustafasaifee.com' : 'https://gendered-toponyms.mustafasaifee.com';
-  const quote = data ?
-    city === 'Berlin' ?
-      `Out of ${data.maleStreets + data.femaleStreets + data.unknown + data.multiple} epynomous streets in #${city}, ${data.maleStreets} are named after males (cis), ${data.femaleStreets - 1} are named after females(cis), 1 is named after transgender females amd 1 is named after 2 or more people with different genders via Gendered Toponyms by @mustafasaifee42.` :
-      `Out of ${data.maleStreets + data.femaleStreets + data.unknown + data.multiple} epynomous streets in #${city}, ${data.maleStreets} are named after males, ${data.femaleStreets} are named after females${data.multiple > 0 ? `, ${data.multiple} ${data.multiple > 1 ? 'are' : 'is'} named after 2 or more people with different genders` : ''}${data.unknown > 0 ? `, and for ${data.unknown} ${data.unknown > 1 ? 'streets' : 'street'} genders can't be determined` : ''} via Gendered Toponyms by @mustafasaifee42.`
-    :
-    'Gendered Toponyms: Mapping gender imbalance in city street names by @mustafasaifee42.';
+  const url = selectedCity
+    ? cityList.indexOf(selectedCity) > -1
+      ? `https://gendered-toponyms.mustafasaifee.com/?city=${selectedCity}`
+      : "https://gendered-toponyms.mustafasaifee.com"
+    : "https://gendered-toponyms.mustafasaifee.com";
+  const quote = data
+    ? city === "Berlin"
+      ? `Out of ${
+          data.maleStreets + data.femaleStreets + data.unknown + data.multiple
+        } epynomous streets in #${city}, ${
+          data.maleStreets
+        } are named after males(cis), ${
+          data.femaleStreets - 1
+        } are named after females(cis), 1 is named after transgender females and 1 is named after multiple people with different genders via Gendered Toponyms by @mustafasaifee42.`
+      : `Out of ${
+          data.maleStreets + data.femaleStreets + data.unknown + data.multiple
+        } epynomous streets in #${city}, ${
+          data.maleStreets
+        } are named after males, ${data.femaleStreets} are named after females${
+          data.multiple > 0
+            ? `, ${data.multiple} ${
+                data.multiple > 1 ? "are" : "is"
+              } named after multiple people with different genders`
+            : ""
+        }${
+          data.unknown > 0
+            ? `, and for ${data.unknown} ${
+                data.unknown > 1 ? "streets" : "street"
+              } genders can't be determined`
+            : ""
+        } via Gendered Toponyms by @mustafasaifee42.`
+    : "Gendered Toponyms: Mapping gender imbalance in city street names by @mustafasaifee42.";
   return (
     <TwitterShareButton url={url} title={quote}>
-      <TwitterIcon size={24} round={true} bgStyle={{ fill: `${darkMode ? '#999999' : '#AAAAAA'}` }} />
+      <TwitterIcon
+        size={24}
+        round={true}
+        bgStyle={{ fill: `${darkMode ? "#999999" : "#AAAAAA"}` }}
+      />
     </TwitterShareButton>
-  )
-}
+  );
+};
 
-export default ShareEl
+export default ShareEl;
